1c77e9362c22564ec607948462f671d72c0210c5
Plut0r <ogunolaololade@gmail.com>
2023-11-20 10:53:24 +0100

    feat: authenticated-header

--- pages/test/header.tsx
+++
@@ -1,8 +0,0 @@
-import AuthenticatedHeader from '@/components/components/authenticatedheader';
-import React from 'react';
-
-function TestHeader() {
-  return <AuthenticatedHeader />;
-}
-
-export default TestHeader;
