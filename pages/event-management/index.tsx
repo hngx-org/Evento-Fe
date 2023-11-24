5b3714546500ecff77738aa3a4de89959fd2b97c
Toheeb Opeyemi AJAYI <toheebopeyemi9@gmail.com>
2023-11-24 19:21:12 +0100

    Responsiveness

--- pages/event-management/index.tsx
+++ pages/event-management/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import EventManagementLayout from '@/layout/EventManagementLayout';
+'use client';
 
-export default function index() {
-  return <EventManagementLayout>Event Management</EventManagementLayout>;
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Index() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/event-management/event-overview');
+  }, [router]);
+
+  return null;
 }
