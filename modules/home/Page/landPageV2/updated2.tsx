13676de00ec509e5ff34fe60b844af93e7ad00a1
Phoenix <ayomikuntemitope.araoye@gmail.com>
2023-11-24 22:18:13 +0200

    fixes

--- modules/home/Page/landPageV2/updated2.tsx
+++ modules/home/Page/landPageV2/updated2.tsx
@@ -5,7 +5,7 @@ import overlay from '@/public/land1.svg';
 
 const Hpmenewv2 = () => {
   return (
-    <section className="px-4 justify-center  items-center my-6 mt-12 md:mt-0">
+    <section className="px-4 justify-center  items-center my-4 mt-12 md:mt-0">
       <div className=" justify-center flex items-center">
         <div className="w-1/2 py-4 px-9">
           <div className="relative">
@@ -13,19 +13,7 @@ const Hpmenewv2 = () => {
               <Image src={overlay} className="mr-6" alt={'overlay'} />
             </div>
             <div className="relative">
-              <h1 className="text-5xl hidden sm:flex font-bold mb-5 text-gray-900">
-                Crafting Experiences <br /> One <span className="text-primary-100">Event</span> at a Time
-              </h1>
-              <h1
-                className="text-5xl sm:hidden font-bold mb-5 text-gray-900"
-                style={{
-                  backgroundImage: `url(${overlay})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'right',
-                  backgroundRepeat: 'no-repeat',
-                  paddingRight: '8rem',
-                }}
-              >
+              <h1 className="text-5xl font-bold mb-5 text-gray-900">
                 Crafting Experiences <br /> One <span className="text-primary-100">Event</span> at a Time
               </h1>
             </div>
@@ -44,7 +32,7 @@ const Hpmenewv2 = () => {
             Create an Event
           </Button>
         </div>
-        <div className="w-1/2 sm:hidden  py-4 px-9">
+        <div className="w-1/2 hidden lg:flex  py-4 px-9">
           <Image className="rounded-3xl" src={top} alt="img" width={500} height={300} />
         </div>
       </div>
