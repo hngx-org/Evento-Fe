189f8688f0989173b12bd3f6cf47b64b8c475855
theCephas <oshoiseoluwa@gmail.com>
2023-11-28 17:02:50 +0100

    Fixed up the verify email page

--- pages/verify-email/index.tsx
+++ pages/verify-email/index.tsx
@@ -1,74 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import Button from '@ui/NewButton';
-import Modal from '@/components/ui/Modal';
-import AuthTitle from '@/components/components/authTitle';
+import React from 'react';
+import Logo from '../../public/logo.svg';
+import AppStore from '../../public/app-store.svg';
+import Google from '../../public/g-play.svg';
 import Image from 'next/image';
-import { signUpWithGoogle } from '@/http/authapi';
+import X from '../../public/event-management/x.svg';
+import Insta from '../../public/event-management/instagram.svg';
+import Facebook from '../../public/event-management/facebook.svg';
+import Linkedin from '../../public/assets/linkedin.svg';
 
-function VerifyEmail({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
-  const [modOpen, setOpen] = useState(false);
-  const onOpen = () => setOpen(true);
-  const isClose = () => setOpen(false);
-
-  const [isloading, setIsLoading] = useState(false);
-
-  const handleLinkClick = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      window.location.href = 'https://evento-qo6d.onrender.com/api/v1/google';
-    }, 5000);
-  };
-
+const VerifyEmail = () => {
   return (
-    // <Modal closeOnOverlayClick isOpen={isOpen} closeModal={onClose} size="sm">
-    <div>
-      <div className="relative flex flex-col items-center justify-center m-auto bg-[#fff] w-[auto] sm:w-[700px] rounded-[16px]">
-        <button onClick={onClose} className="absolute top-[46px] right-12">
-          <Image src="/close-circle.svg" alt="Close icon" width={20} height={20} />
-        </button>
-        <div className="p-4 pt-20">
-          <p className="text-[#000] text-[36px] font-[600] ">Enter Code</p>
-          <p className="text-[#000] text-[20px] pt-6 pb-2 font-[400] ">Please enter the 6 digit code we sent to</p>
-          <p className="text-[#000] text-[20px] pb-2 font-[400] ">Youremail@gmail.com.</p>
-          <div className="flex gap-4 sm:gap-8 my-4">
-            <input
-              //   type="number"
-              className="border outline-none text-center text-[20px] border-[#111] rounded-[12px] w-[40px] h-[40px] sm:w-[56px] sm:h-[56px]"
-            />
-            <input
-              //   type="number"
-              className="border outline-none text-center text-[20px] border-[#111] rounded-[12px] w-[40px] h-[40px] sm:w-[56px] sm:h-[56px]"
-            />
-            <input
-              //   type="number"
-              className="border outline-none text-center text-[20px] border-[#111] rounded-[12px] w-[40px] h-[40px] sm:w-[56px] sm:h-[56px]"
-            />
-            <input
-              //   type="number"
-              className="border outline-none text-center text-[20px] border-[#111] rounded-[12px] w-[40px] h-[40px] sm:w-[56px] sm:h-[56px]"
-            />
-            <input
-              //   type="number"
-              className="border outline-none text-center text-[20px] border-[#111] rounded-[12px] w-[40px] h-[40px] sm:w-[56px] sm:h-[56px]"
-            />
-            <input
-              //   type="number"
-              className="border outline-none text-center text-[20px] border-[#111] rounded-[12px] w-[40px] h-[40px] sm:w-[56px] sm:h-[56px]"
-            />
+    <div className="p-6 sm:p-10">
+      <div className="flex flex-col w-auto md:w-[670px] text-center items-center m-auto pt-[106px] pr-[45px] pb-[110.5px] pl-[31px] border border-[#b7b7b7] ">
+        <Image src={Logo} width={0} alt="Logo" />
+        <p className="text-[32px] font-[600] text-[#0f0f0f] leading-[40px] py-8 ">You are almost there!</p>
+        <div className="leading-[24px] text-[14px] font-[400] text-[#0f0f0f] ">
+          <p className="pb-6">Hello John Doe,</p>
+          <p className="pb-6">Welcome to Evento</p>
+          <p className="pb-1">To get started, we need to verify your email address. </p>
+          <p className="pb-6">Please click the link below</p>
+          <p className="pb-10 text-[#360789] cursor-pointer">[Verification Link]</p>
+          <p className="pb-2">Unleash the Extraordinary, </p>
+          <p className="pb-8">Where moments becomes memory</p>
+          <p className="pb-4">Get the Eclipse app to plan your events and stay up-to-date with details on the go.</p>
+          <p>Download the app</p>
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <button>
+            <Image src={AppStore} width={0} alt="App store" />
+          </button>
+          <button>
+            <Image src={Google} width={0} alt="Google Play" />
+          </button>
+        </div>
+        <div className="m-auto flex flex-col items-center mt-10">
+          <Image src={Logo} width={0} alt="Logo" />
+          <div className="flex justify-center items-center gap-4 mt-6">
+            <Image src={Linkedin} width={30} alt="Logo" />
+            <Image src={X} width={30} alt="Logo" />
+            <Image src={Facebook} width={30} alt="Logo" />
+            <Image src={Insta} width={30} alt="Logo" />
           </div>
-          <Button
-            onClick={onOpen}
-            className="px-12 py-4 bg-[#0F0F0F] rounded-lg border border-neutral-900 w-full flex items-center gap-[10px] justify-center"
-          >
-            <p className="text-center text-[#fff] text-base font-medium leading-normal">Log in</p>
-          </Button>
-          <p className="pt-4 text-[16px] font-[400] text-[#111]">
-            Did not receive Code? <span className="font-[600] text-[#000]"> Resend Code</span>
-          </p>
+          <div>
+            <p className="text-[10px] py-2 font-[400] leading-4 text-[#0f0f0f] ">
+              Copyright (C) 2023 Eclipse. All rights reserved.{' '}
+            </p>
+            <p className="text-[10px] font-[400] leading-4 text-[#0f0f0f] ">
+              You are receiving this email because you opted in via our website.
+            </p>
+            <p className="text-[10px] font-[400] leading-4 text-[#0f0f0f] pt-6 pb-4 ">Our mailing address is: </p>
+            <p className="text-[10px] font-[400] leading-4 text-[#0f0f0f] ">
+              Eclipse | 166 Parkview St Ikoyi, Lagos State, Nigeria
+            </p>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default VerifyEmail;
