774320088ec0aeb40fdc0e47c256a7430a8cddc6
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2023-11-30 18:37:23 +0000

    Update index.tsx

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,12 +1,19 @@
-import { Edit } from 'iconsax-react';
+import React, { useEffect, useState } from 'react';
 
 import Button from '@/components/ui/Button';
-import Input from '@/components/UserProfile/Input';
-import AuthLayout from '@/layout/Authlayout';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+
 import { Montserrat, Nunito } from 'next/font/google';
-import { UserProfile, UserProfile2, editUserProfile } from '@/http/profileapi';
+
+import withAuth from '@/helpers/withAuth';
+import AuthLayout from '@/layout/Authlayout';
+import { Edit } from 'iconsax-react';
+import ProfieEvent from '@/components/UserProfile/ProfieEvent';
+
+import { FacebookIcon, InstagramIcon, TwitterIcon } from '@/public/assets/profile/icons';
+import { useRouter } from 'next/router';
+
+import { UserProfile, getUserProfile } from '@/http/profileapi';
+import Image from 'next/image';
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -20,41 +27,47 @@ const montserrat = Montserrat({
   variable: '--font-montserrat',
 });
 
-const EditProfilePage = () => {
-  const router = useRouter();
-
-  const [formData, setFormData] = useState<UserProfile2>({});
-  const [reRoute, setReRoute] = useState(false);
+const UserProfile: React.FC = () => {
+  const [userProfile, setUserProfile] = useState<UserProfile>({
+    userID: '',
+    email: '',
+    bio: '',
+    socialLinks: {
+      facebook: '',
+      instagram: '',
+      twitter: '',
+      website: '',
+    },
+    profileImage: '',
+    displayName: '',
+    firstName: '',
+    lastName: '',
+    slug: '',
+    role: '',
+    location: '',
+  });
 
   useEffect(() => {
-    if (reRoute) {
-      router.push('/profile');
-    }
-  }, [reRoute, router]);
+    getUserProfile(setUserProfile);
 
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>,
-    imageBlob?: Blob,
-  ) => {
-    const { name, value } = e.target;
-
-    if (name === 'twitter' || name === 'facebook' || name === 'instagram') {
-      setFormData((prevData) => ({
-        ...prevData,
-        socialLinks: {
-          ...prevData?.socialLinks,
-          [name]: value,
-        },
-      }));
-    } else {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }
-  };
+    const demo = {
+      userID: '409a273a-be65-436f-9e76-b914ee3be5ca',
+      email: 'halesh553@gmail.com',
+      bio: null,
+      socialLinks: null,
+      profileImage: null,
+      displayName: null,
+      firstName: 'Ibukun',
+      lastName: 'Alesinloye',
+      slug: 'ibukun-alesinloye-1',
+      role: 'USER',
+      location: null,
+    };
+  }, []);
 
   const [profilePicURL, setProfilePicURL] = useState('');
+
+  const router = useRouter();
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedImage = event.target.files && event.target.files[0];
@@ -62,8 +75,9 @@ const EditProfilePage = () => {
     if (selectedImage) {
       // Log the name of the selected image file
       const imageBlob = new Blob([selectedImage], { type: selectedImage.type });
-
-      setFormData((prevData) => ({ ...prevData, profileImage: selectedImage }));
+      // post
+      // postProfilePicture(imageBlob);
+      // ?post
 
       const imageUrl = URL.createObjectURL(selectedImage);
       setProfilePicURL(imageUrl);
@@ -80,171 +94,93 @@ const EditProfilePage = () => {
     // handle the posting here
   };
 
-  const submitForm = (e: React.FormEvent<HTMLFormElement> | undefined) => {
-    console.log(formData);
-    editUserProfile(formData, () => {
-      setReRoute(true); // Trigger rerouting upon successful profile update
-    });
-  };
   return (
     <AuthLayout>
-      <form
-        className={` ${nunito.className} flex justify-center w-full h-fit min-h-screen bg-[#F5F5F5] pt-[40px] pb-[55px] md:pt-[64px]   md:pb-[219px] lg:pt-[107px]  lg:pb-[377px] flex-col items-center gap-y-[32px]  overflow-hidden `}
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(e);
-          submitForm(e);
-        }}
-      >
-        <section className=" w-[358px] md:w-[634px] lg:w-[842px] bg-white-100 relative p-6 lg:p-[64px] flex flex-col gap-y-[24px]  rounded-2xl">
-          <div
-            id="profilePicContainer"
-            className=" rounded-[50%] w-[120px] h-[120px] bg-[#A4A4A4]  flex  justify-center items-center overflow-hidden z-[1] "
+      <div className={` ${nunito.className} flex w-[100vw] overflow-hidden justify-center bg-[#F5F5F5]  `}>
+        <section className="w-full h-[240px] bg-secondary-100 absolute ">
+          <Button
+            handleClick={() => {}}
+            styles={'  !rounded-[50%] border border-[#ED9E72] relative right-20 top-6 float-right'}
+            type={'button'}
+            title={'edit profile card'}
+            disabled={true}
           >
-            <Edit onClick={() => {}} />
-            <input
-              type="file"
-              accept="image/*"
-              onChange={(event) => {
-                handleImageChange(event);
-              }}
-              className=" absolute w-6 h-6 opacity-0 z-0 cursor-pointer"
-            />
-          </div>
-
-          <div className="info flex flex-col gap-4">
-            <div className="flex flex-col gap-1">
-              <h6 className={` ${montserrat.className}  text-xl font-bold`}>Edit Profile</h6>
-              <p className="text-base line-clamp-3 text-[#959595]">Make changes to your profile</p>
-            </div>
-            <div className="personalInfo flex flex-col gap-y-5 ">
-              <div className="flex flex-col md:flex-row gap-y-8 justify-between ">
-                <div className=" md:w-[277px] lg:w-[341px] ">
-                  {' '}
-                  <Input
-                    label="First name "
-                    placeholder="Enter Firstname"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                  ></Input>
-                </div>
-                <div className=" md:w-[277px] lg:w-[341px] ">
-                  {' '}
-                  <Input
-                    label="Last name"
-                    placeholder="Enter Last name"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleInputChange}
-                  ></Input>
-                </div>
-              </div>
-              {/* <div className="w-[380px] ">
-              {' '}
-              <Input label="Display Name" type="text" placeholder="Enter a unique username"></Input>
-            </div> */}
-              <div placeholder="Enter Firstname">
-                {' '}
-                <Input
-                  label="Short bio "
-                  inputHeight="min-h-[104px]"
-                  textArea={true}
-                  placeholder="Enter a short bio"
-                  name="bio"
-                  value={formData.bio}
-                  onChange={handleInputChange}
-                ></Input>
-              </div>
-            </div>
-          </div>
-
-          <div className="info flex flex-col gap-4">
-            <div className="flex flex-col gap-1">
-              <h6 className={` ${montserrat.className} text-xl font-bold`}>Social Links</h6>
-              <p className="text-base line-clamp-3 text-[#959595]">
-                Add existing social links to build a stronger reputation
-              </p>
-            </div>
-            <div className="socialInfo flex flex-col gap-y-5 ">
-              <div className="flex flex-col md:flex-row gap-y-8 justify-between ">
-                <div className=" md:w-[277px] lg:w-[341px] ">
-                  {' '}
-                  <Input
-                    label="Website URL"
-                    placeholder="Enter your website URL"
-                    name="website"
-                    value={formData?.socialLinks?.website}
-                    onChange={handleInputChange}
-                  ></Input>
-                </div>
-                <div className=" md:w-[277px] lg:w-[341px] ">
-                  {' '}
-                  <Input
-                    label="Twitter"
-                    placeholder="Enter your twitter handle"
-                    name="twitter"
-                    value={formData?.socialLinks?.twitter}
-                    onChange={handleInputChange}
-                  ></Input>
-                </div>
-              </div>
-              <div className="flex flex-col md:flex-row gap-y-8 justify-between ">
-                <div className=" md:w-[277px] lg:w-[341px] ">
-                  {' '}
-                  <Input
-                    label="Facebook"
-                    placeholder="Enter your facebook handle"
-                    name="facebook"
-                    value={formData?.socialLinks?.facebook}
-                    onChange={handleInputChange}
-                  ></Input>
-                </div>
-                <div className=" md:w-[277px] lg:w-[341px] ">
-                  {' '}
-                  <Input
-                    label="Instagram"
-                    placeholder="Enter your instagram handle"
-                    name="instagram"
-                    value={formData?.socialLinks?.instagram}
-                    onChange={handleInputChange}
-                  ></Input>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="button flex flex-col md:flex-row gap-[32px]  lg:self-center">
-            <Button
-              handleClick={() => {
-                // closeModal();
-                // route back to peofile page
-                console.log('close modal');
-                router.push('/profile');
-              }}
-              styles={
-                '!bg-white-100 gap-2 text-primary-100 border border-primary-100 w-full md:w-[285px] lg:w-[187px] py-4  '
-              }
-              type={'button'}
-              title={'edit profile'}
-              disabled={false}
-            >
-              Cancel
-            </Button>
-            <Button
-              styles={' text-white-100  w-full md:w-[187px] py-4 '}
-              type={'submit'}
-              title={'save profile'}
-              disabled={false}
-            >
-              Save profile
-            </Button>
-          </div>
+            <Edit color="#FCEEE7" fontSize={20} className="" />
+          </Button>
         </section>
 
-        {/* <section className="events w-[906px] relative "></section> */}
-      </form>
+        <section className="w-[358px] md:w-[634px] lg:w-[906px] relative top-[120px] flex flex-col gap-y-[92px] mb-[40vh]">
+          <div className="w-full flex flex-col gap-y-6  bg-white-100 rounded-[12px] p-6">
+            {userProfile.profileImage ? (
+              <div
+                id="profilePicContainer"
+                className=" rounded-[50%] w-[120px] h-[120px] bg-[#A4A4A4]  flex  justify-center items-center overflow-hidden "
+              >
+                <Image
+                  alt=""
+                  src="https://res.cloudinary.com/dnc0fjkpo/image/upload/v1701274125/ftxvhcqapviuntzypts5.png"
+                  width={120}
+                  height={120}
+                />
+              </div>
+            ) : (
+              <div
+                id="profilePicContainer"
+                className=" rounded-[50%] w-[120px] h-[120px] bg-[#A4A4A4]  flex  justify-center items-center overflow-hidden "
+              >
+                <Edit onClick={() => {}} />
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={(event) => {
+                    handleImageChange(event);
+                  }}
+                  className=" absolute w-6 h-6 opacity-0"
+                />
+              </div>
+            )}
+
+            <div className="info flex flex-col gap-4">
+              <div className="flex justify-between">
+                <h6 className={`${montserrat.className} text-xl md:text-2xl font-bold whitespace-nowrap1 `}>
+                  {userProfile?.lastName + ' ' + userProfile?.firstName}
+                </h6>
+                <Button
+                  handleClick={() => {
+                    router.push('/profile/edit');
+                    console.log('open Modal');
+                  }}
+                  styles={`${nunito.className} !font-bold !bg-white-100 flex items-center whitespace-nowrap gap-2 text-primary-100 border border-primary-100 py-2 px-3 lg:text-[14px] text-[12px]`}
+                  type={'button'}
+                  title={'edit profile'}
+                  disabled={false}
+                >
+                  <Edit className="h-4 w-4" />
+                  Edit Profile
+                </Button>
+              </div>
+              <p className=" text-xs md:text-base line-clamp-3 max-w-full">{userProfile?.bio}</p>
+              <div className="socials flex gap-x-[20px] items-center">
+                <a href={userProfile?.socialLinks?.instagram} target="_blank" rel="noopener noreferrer">
+                  <InstagramIcon />
+                </a>
+
+                <a href="https://www.facebook.com/your_facebook_account" target="_blank" rel="noopener noreferrer">
+                  <FacebookIcon />
+                </a>
+
+                <a href="https://twitter.com/your_twitter_account" target="_blank" rel="noopener noreferrer">
+                  <TwitterIcon />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <ProfieEvent />
+        </section>
+      </div>
     </AuthLayout>
   );
 };
 
-export default EditProfilePage;
+export default withAuth(UserProfile);
