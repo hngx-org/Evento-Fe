54459df293eefcacf9fc3d9f3515d39f71edc23a
Toheeb Opeyemi AJAYI <toheebopeyemi9@gmail.com>
2023-12-01 20:45:05 +0100

    Event Management Fixed

--- pages/event-management/event-overview/index.tsx
+++ pages/event-management/event-overview/index.tsx
@@ -1,8 +1,27 @@
 'use client';
 
 import React, { useRef } from 'react';
+import { Work_Sans, Nunito, Montserrat } from 'next/font/google';
 import EventManagementLayout from '@/layout/EventManagementLayout';
 import Image from 'next/image';
+
+const workSans = Work_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-work-sans',
+});
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-nunito',
+});
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat',
+});
 
 const event = {
   name: 'Event Name',
@@ -36,11 +55,21 @@ export default function Index() {
     <EventManagementLayout>
       {/* End Image */}
       <div className="mt-[52px] sm:mt-10 relative">
-        <button className="absolute top-4 left-4 px-4 py-[6px] rounded-md bg-Grey-G40 text-xl font-semibold">
+        <button
+          type="button"
+          className={`${nunito.className} absolute top-6 left-6 px-4 py-[6px] rounded-md bg-Grey-G40 text-xl font-semibold`}
+        >
           {event?.type}
         </button>
-        {/* The Immediate Div after this comment should be replace with the actual event Image during API consumption */}
-        <div className="w-ful h-[433px] bg-[F5F5F5] sm:bg-[#00000033] rounded-2xl border border-Grey-G40"></div>
+        <div className="w-ful h-[433px] rounded-2xl overflow-hidden">
+          <Image
+            src={'/event-management/frame1.svg'}
+            height={1128}
+            width={1692}
+            alt="Event Image"
+            className="w-full h-full object-cover"
+          />
+        </div>
         <button
           type="button"
           title="Edit Event Image"
@@ -50,9 +79,11 @@ export default function Index() {
         </button>
       </div>
       {/* Event Name */}
-      <h1 className="mt-8 sm:mt-10 text-2xl sm:text-[32px] font-bold text-Grey-G600">{event?.name}</h1>
+      <h1 className={`${montserrat.className} mt-8 sm:mt-10 text-2xl sm:text-[32px] font-bold text-Grey-G600`}>
+        {event?.name}
+      </h1>
       {/* Description */}
-      <div className="mt-4 sm:mt-6 text-xl text-Grey-G300 grid gap-5">
+      <div className={`${nunito.className} mt-4 sm:mt-6 text-xl text-Grey-G300 grid gap-5`}>
         <p>{event?.description1}</p>
         <p>{event?.description2}</p>
         <p>{event?.description3}</p>
@@ -64,14 +95,14 @@ export default function Index() {
         <div className="flex items-start gap-2">
           <Image src={'/event-management/location.svg'} width={24} height={24} className="mt-2" alt="Calender Icon" />
           <div>
-            <h3 className="text-2xl font-medium text-primary-100">Location of event:</h3>
-            <p className="mt-2 text-xl text-gray-fot">{event?.location}</p>
+            <h3 className={`${montserrat.className} text-2xl font-medium text-primary-100`}>Location of event:</h3>
+            <p className={`${nunito.className} mt-2 text-xl font-semibold text-gray-fot`}>{event?.location}</p>
           </div>
         </div>
         <div className="grid sm:grid-cols-2 gap-8 sm:gap-6">
           {/* Start */}
           <div>
-            <div className="flex gap-1 items-center">
+            <div className="flex gap-1 items-center -ms-2">
               <Image
                 src={'/event-management/calender.svg'}
                 width={40}
@@ -79,16 +110,16 @@ export default function Index() {
                 // className='w-6 h-7'
                 alt="Calender Icon"
               />
-              <h3 className="text-2xl font-medium text-primary-100">Start date:</h3>
+              <h3 className={`${montserrat.className} text-2xl font-medium text-primary-100`}>Start date:</h3>
             </div>
-            <div className="px-4 mt-2 text-xl font-semibold">
+            <div className={`${nunito.className} mt-2 text-xl font-semibold`}>
               <h5 className="text-gray-fot">{event?.start_date}</h5>
               <p className="mt-2 text-Grey-G70">{event?.start_time}</p>
             </div>
           </div>
           {/* End */}
           <div>
-            <div className="flex gap-1 items-center">
+            <div className="flex gap-1 items-center -ms-2">
               <Image
                 src={'/event-management/calender.svg'}
                 width={40}
@@ -96,28 +127,31 @@ export default function Index() {
                 // className='w-6 h-7'
                 alt="Calender Icon"
               />
-              <h3 className="text-2xl font-medium text-primary-100">End date:</h3>
+              <h3 className={`${montserrat.className} text-2xl font-medium text-primary-100`}>End date:</h3>
             </div>
-            <div className="px-4 mt-2 text-xl font-semibold">
+            <div className={`${nunito.className} mt-2 text-xl font-semibold`}>
               <h5 className="text-gray-fot">{event?.end_date}</h5>
               <p className="mt-2 text-Grey-G70">{event?.end_time}</p>
             </div>
           </div>
         </div>
-        {/* Location */}
       </div>
 
       {/* Category, Capacity, Type */}
-      <div className="mt-8 grid grid-cols-2 sm:grid-cols-4 gap-8 text-center sm:text-left">
-        <div>
-          <h5 className="text-xl sm:text-2xl font-medium text-Grey-G70">Event category</h5>
-          <p className="mt-2 text-xl text-gray-fot">{event?.category}</p>
+      <div className="mt-8 flex flex-col sm:flex-row gap-4 md:gap-10 sm:items-center">
+        <div className="w-full sm:max-w-[466px] flex justify-between sm:justify-start gap-4 md:gap-10 items-center">
+          <div className="w-full max-w-[193px]">
+            <h5 className="text-xl sm:text-2xl font-medium text-Grey-G70">Event category</h5>
+            <p className="mt-2 text-xl text-gray-fot">{event?.category}</p>
+          </div>
+          <div className="w-[2px] h-[88px] bg-[#EBEBEB] rounded-full"></div>
+          <div className="w-full max-w-[193px]">
+            <h5 className="text-xl sm:text-2xl font-medium text-Grey-G70">Event capacity</h5>
+            <p className="mt-2 text-xl text-gray-fot">{event?.capacity}</p>
+          </div>
         </div>
-        <div>
-          <h5 className="text-xl sm:text-2xl font-medium text-Grey-G70">Event capacity</h5>
-          <p className="mt-2 text-xl text-gray-fot">{event?.capacity}</p>
-        </div>
-        <div className="col-span-2 sm:col-auto">
+        <div className="w-full sm:w-[2px] h-[2px] sm:h-[88px] bg-[#EBEBEB] rounded-full"></div>
+        <div className="w-full sm:max-w-[193px]">
           <h5 className="text-xl sm:text-2xl font-medium text-Grey-G70">Event Ticket type</h5>
           <p className="mt-2 text-xl text-gray-fot">{event?.type}</p>
         </div>
@@ -126,17 +160,17 @@ export default function Index() {
       {/* Event Link and Host */}
       <div className="mt-8 flex flex-col-reverse sm:flex-col gap-8 ">
         <div className="w-full flex flex-col sm:flex-row sm:items-center gap-4 sm:gap-2">
-          <h5 className="text-primary-100 text-2xl font-semibold">Host:</h5>
-          <div className="font-medium">
+          <h5 className={`${montserrat.className} text-primary-100 text-2xl font-medium`}>Host:</h5>
+          <div className={`${nunito.className} font-medium`}>
             <h4 className="text-xl text-Grey-G500">{event?.host_name}</h4>
             {/* <p className="mt-1 text-Grey-G70">{event?.host_email}</p> */}
           </div>
         </div>
         <div className="w-full flex flex-col sm:flex-row sm:items-center gap-4 sm:gap-2">
-          <h5 className="text-primary-100 text-2xl font-medium">Event link:</h5>
+          <h5 className={`${montserrat.className} text-primary-100 text-2xl font-medium`}>Event link:</h5>
           <div className="sm:max-w-[257px] w-full relative">
             <input
-              className="w-full py-3 px-4 rounded-lg border border-Success-S500 text-Success-S500 bg-Success-S50 font-bold"
+              className={`${nunito.className} w-full py-3 px-4 rounded-lg border border-Success-S500 text-Success-S500 bg-Success-S50 font-bold`}
               type="text"
               value={event?.link}
               title="Event Link"
@@ -159,10 +193,10 @@ export default function Index() {
       <div className="mt-8 sm:mt-20">
         <button
           type="button"
-          className="max-w-[411px] w-full mx-auto py-3 px-[18px] text-primary-100 bg-white-N0 border border-primary-100 rounded-lg flex justify-center items-center gap-2 font-bold"
+          className="sm:max-w-[411px] w-full mx-auto py-3 px-[18px] text-primary-100 bg-white-N0 border border-primary-100 rounded-lg flex justify-center items-center gap-2 font-bold"
         >
-          <Image src={'/event-management/edit-2.svg'} width={20} height={20} alt="Edit" />
-          <span>Edit Event</span>
+          <Image src={'/event-management/edit.svg'} width={20} height={20} alt="Edit" />
+          <span className={nunito.className}>Edit Event</span>
         </button>
       </div>
     </EventManagementLayout>
