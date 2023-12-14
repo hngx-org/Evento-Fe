5dc8d8e68ad049a494cb15b6ac3d9eaa249ecc5c
Nwonye-Michael <49059581+Nwonye-Michael@users.noreply.github.com>
2023-12-14 14:14:36 +0100

    added my modal

--- pages/eventdashboard3/index.tsx
+++ pages/eventdashboard3/index.tsx
@@ -20,6 +20,8 @@ import Button from '@/components/ui/Button';
 import { Add, Discover } from 'iconsax-react';
 import EventCalender from '@/components/ui/Calender';
 import EventCalender3 from '@/components/EventDashBoard3/EventCalendar2';
+import Modal from '@/components/ui/Modal';
+import ListEventCard from '@/components/UserProfile/ListEventCard';
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -52,8 +54,8 @@ const Dashboard3: React.FC = () => {
   const [createdEvents, setCreatedEvent] = useState<eventType[]>([]);
   const [upcomingEvents, setUpcomingEvents] = useState<eventType[]>([]);
   const combinedEvents = [...createdEvents, ...upcomingEvents];
-  // const [combinedViewMore, setCombinedViewMore] = useState(false);
-  // const [pastViewMore, setPastViewMore] = useState(false);
+  const [combinedViewMore, setCombinedViewMore] = useState(false);
+  const [pastViewMore, setPastViewMore] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -73,12 +75,48 @@ const Dashboard3: React.FC = () => {
 
   return (
     <AuthLayout>
-      {/* desktop and tab */}
-      <div className={`lg:px-20 md:pl-6 px-[15px] mt-10   ${nunito.className} max-h-[100vh]`}>
+      {/* modals */}
+      <Modal
+        isOpen={combinedViewMore}
+        closeModal={function (): void {
+          setCombinedViewMore(false);
+        }}
+        closeOnOverlayClick={true}
+        size="xxl"
+      >
+        <div className="max-h-[500px] overflow-clip overflow-y-scroll  no-scrollbar ">
+          {' '}
+          <div className=" relative w-fit grid grid-cols-1 md:grid-cols-1 gap-6 max-h-full md:px-[40px] lg:px-[85px] py-10 overflow-y-scroll no-scrollbar ">
+            {combinedEvents.map((event, index) => (
+              <GridEventCard key={index} event={event} past={false} />
+              // <ListEventCard key={index} event={event} past={false} />
+            ))}
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={pastViewMore}
+        closeModal={function (): void {
+          setPastViewMore(false);
+        }}
+        closeOnOverlayClick={true}
+        size="xxl"
+      >
+        <div className="max-h-[500px] overflow-clip overflow-y-scroll  no-scrollbar ">
+          {' '}
+          <div className=" relative w-fit grid grid-cols-1 md:grid-cols-1 gap-6 max-h-full md:px-[40px] lg:px-[85px] py-10 overflow-y-scroll no-scrollbar">
+            {pastEvents.map((event, index) => (
+              <GridEventCard key={index} event={event} past={true} />
+              // <ListEventCard key={index} event={event} past={true} />
+            ))}
+          </div>
+        </div>
+      </Modal>
+      <div className={`lg:px-20 md:pl-6 px-[15px] my-10   ${nunito.className} `}>
         <section className="mb-14 ">
           <div className={`text-2xl font-medium mb-10 ${montserrat.className} `}>Welcome {userProfile.firstName},</div>
           <div className="upcomingRest flex flex-wrap lg:justify-between gap-8 md:gap-10 w-full ">
-            <div className="w-[163px]  md:w-[360px] md:max-w-[calc(50%-40px)]  lg:max-w-none flex flex-shrink-0 justify-between p-6 rounded-lg whitespace-nowrap shadow-[0px_1px_2px_0px_rgba(16,24,40,0.06),0px_1px_3px_0px_rgba(16,24,40,0.10)]">
+            <div className="w-[163px] max-w-[calc(50%-16px)]  md:w-[360px] md:max-w-[calc(50%-40px)]  lg:max-w-none flex flex-shrink-0 justify-between p-6 rounded-lg whitespace-nowrap shadow-[0px_1px_2px_0px_rgba(16,24,40,0.06),0px_1px_3px_0px_rgba(16,24,40,0.10)]">
               <div className="flex flex-col gap-3 justify-between">
                 <span className={` text-2xl md:text-[32px] md:leading-[40px] font-bold ${montserrat.className} `}>
                   {' '}
@@ -89,7 +127,7 @@ const Dashboard3: React.FC = () => {
 
               <Image src={upcoming} alt={''} className="hidden md:flex" />
             </div>
-            <div className="w-[163px]  md:w-[360px] md:max-w-[calc(50%-40px)]  lg:max-w-none flex flex-shrink-0 justify-between p-6 rounded-lg whitespace-nowrap shadow-[0px_1px_2px_0px_rgba(16,24,40,0.06),0px_1px_3px_0px_rgba(16,24,40,0.10)]">
+            <div className="w-[163px] max-w-[calc(50%-16px)]  md:w-[360px] md:max-w-[calc(50%-40px)]  lg:max-w-none flex flex-shrink-0 justify-between p-6 rounded-lg whitespace-nowrap shadow-[0px_1px_2px_0px_rgba(16,24,40,0.06),0px_1px_3px_0px_rgba(16,24,40,0.10)]">
               <div className="flex flex-col gap-3 justify-between">
                 <span className={`text-2xl md:text-[32px] md:leading-[40px]  font-bold ${montserrat.className} `}>
                   {' '}
@@ -101,7 +139,7 @@ const Dashboard3: React.FC = () => {
               <Image src={created} alt={''} className="hidden md:flex" />
             </div>
 
-            <div className="w-[163px]  md:w-[360px] md:max-w-[calc(50%-40px)]  lg:max-w-none flex flex-shrink-0 justify-between p-6 rounded-lg whitespace-nowrap shadow-[0px_1px_2px_0px_rgba(16,24,40,0.06),0px_1px_3px_0px_rgba(16,24,40,0.10)]">
+            <div className="w-[163px] max-w-[calc(50%-16px)]  md:w-[360px] md:max-w-[calc(50%-40px)]  lg:max-w-none flex flex-shrink-0 justify-between p-6 rounded-lg whitespace-nowrap shadow-[0px_1px_2px_0px_rgba(16,24,40,0.06),0px_1px_3px_0px_rgba(16,24,40,0.10)]">
               <div className="flex flex-col gap-3 justify-between">
                 <span className={`text-2xl md:text-[32px] md:leading-[40px]  font-bold ${montserrat.className} `}>
                   {' '}
@@ -143,28 +181,14 @@ const Dashboard3: React.FC = () => {
               <NoEvent type={''} />
             ) : (
               <div className="w-full flex flex-col  gap-12 items-center">
-                <div className="upcoming flex flex-col gap-4">
+                <div className="upcoming flex flex-col gap-4 w-fit">
                   {' '}
-                  <div className="upcomingtitle flex justify-between items-baseline ">
+                  <div className="upcomingtitle flex justify-between items-baseline w-full">
                     <span className="text-[#1E1E1E] text-xl font-semibold tracking-[-0.2px] ">Upcoming Events</span>{' '}
                     <span
                       className="text-sm underline  font-bold text-[#767676] hover:scale-105 hover:text-primary-100 cursor-pointer"
                       onClick={() => {
-                        // setCombinedViewMore((prev) => !prev);
-                        router.push('./profile');
-                        setTimeout(() => {
-                          const pastEventsButton = document.getElementById('attended-events');
-
-                          if (pastEventsButton) {
-                            const yPos = pastEventsButton.getBoundingClientRect().top;
-                            window.scrollTo({
-                              top: yPos,
-                              behavior: 'smooth', // Optional: To make the scrolling smooth
-                            });
-                          }
-
-                          //  pastEventsButton.
-                        }, 200);
+                        setCombinedViewMore(true);
                       }}
                     >
                       {/* {combinedViewMore ? 'View less' : 'View all'} */}
@@ -173,11 +197,18 @@ const Dashboard3: React.FC = () => {
                   </div>
                   {upcomingEvents.length > 0 || createdEvents.length > 0 ? (
                     // desktop and tab
-                    <div className=" grid-cols-1 md:grid-cols-2 gap-10 transition w-fit px-[10px] sm:px-[17px] md:px-0 md:grid hidden">
-                      {combinedEvents.slice(0, 2).map((event) => (
-                        <GridEventCard key={0} event={event} past={false} />
-                      ))}
-                    </div>
+                    <>
+                      <div className=" hidden md:grid-cols-2 gap-10 transition w-fit px-[10px] sm:px-[17px] md:px-0 md:grid ">
+                        {combinedEvents.slice(0, 2).map((event, index) => (
+                          <GridEventCard key={index} event={event} past={false} />
+                        ))}
+                      </div>
+                      <div className=" grid md:hidden grid-cols-1 gap-10 transition w-fit px-[10px] sm:px-[17px] md:px-0  ">
+                        {combinedEvents.slice(0, 1).map((event, index) => (
+                          <GridEventCard key={index} event={event} past={false} />
+                        ))}
+                      </div>
+                    </>
                   ) : (
                     <div className="flex gap-y-1 flex-col h-[250px] justify-center items-center w-full">
                       <h4
@@ -206,78 +237,67 @@ const Dashboard3: React.FC = () => {
                 </div>
 
                 <div className="past flex flex-col  gap-4 items-center">
-                  {pastEvents.length < 0 && createdEvents.length < 0 && upcomingEvents.length < 0 ? (
-                    <NoEvent type={''} />
-                  ) : (
-                    <div className="w-fit flex flex-col  gap-4 ">
-                      <div className="pasttitle flex justify-between items-baseline w-full">
-                        <span className="text-[#1E1E1E] text-xl font-semibold tracking-[-0.2px] ">Past Events</span>{' '}
-                        <span
-                          className="text-sm underline  font-bold text-[#767676] hover:scale-105 hover:text-primary-100 cursor-pointer"
-                          onClick={() => {
-                            // console.log('clicked view all');
-                            // setPastViewMore((prev) => !prev);
-
-                            router.push('./profile');
-
-                            setTimeout(() => {
-                              const pastEventsButton = document.getElementById('attended-events');
-                              if (pastEventsButton) {
-                                const yPos = pastEventsButton.getBoundingClientRect().top;
-                                window.scrollTo({
-                                  top: yPos,
-                                  behavior: 'smooth', // Optional: To make the scrolling smooth
-                                });
-                              }
-
-                              pastEventsButton?.click();
-                            }, 200);
-                          }}
-                        >
-                          {/* {pastViewMore ? 'View less' : 'View all'}
-                           */}
-                          View all
-                        </span>
-                      </div>
-
-                      {pastEvents.length > 0 ? (
-                        <div className=" grid-cols-1 md:grid-cols-2 grid gap-10 transition w-fit ">
-                          {pastEvents.slice(0, 2).map((event) => (
-                            <GridEventCard key={0} event={event} past={true} />
-                          ))}
-                        </div>
-                      ) : (
-                        <div className="flex gap-y-1 flex-col h-[250px] justify-center items-center w-full">
-                          <h4
-                            className={` ${montserrat.className} text-2xl w-[196px] md:w-fit  font-semibold  text-center text-[#303030]`}
-                          >
-                            There’s nothing to show here.
-                          </h4>
-
-                          <p className="text-base text-[#767676] max-w-[220px] md:max-w-none w-[196px] md:w-fit text-center">
-                            <span
-                              className="font-bold text-primary-100 cursor-pointer"
-                              onClick={() => router.push('/create-events')}
-                            >
-                              create event
-                            </span>{' '}
-                            or{' '}
-                            <span
-                              className="font-bold text-primary-100 cursor-pointer"
-                              onClick={() => router.push('/explore')}
-                            >
-                              explore event
-                            </span>
-                          </p>
-                        </div>
-                      )}
+                  <div className="w-fit flex flex-col  gap-4 ">
+                    <div className="pasttitle flex justify-between items-baseline w-full">
+                      <span className="text-[#1E1E1E] text-xl font-semibold tracking-[-0.2px] ">Past Events</span>{' '}
+                      <span
+                        className="text-sm underline  font-bold text-[#767676] hover:scale-105 hover:text-primary-100 cursor-pointer"
+                        onClick={() => {
+                          setPastViewMore(true);
+                        }}
+                      >
+                        {/* {pastViewMore ? 'View less' : 'View all'}
+                         */}
+                        View all
+                      </span>
                     </div>
-                  )}
+
+                    {pastEvents.length > 0 ? (
+                      <div className=" grid-cols-1 md:grid-cols-2 grid gap-10 transition w-fit ">
+                        <>
+                          <div className=" hidden md:grid-cols-2 gap-10 transition w-fit px-[10px] sm:px-[17px] md:px-0 md:grid ">
+                            {pastEvents.slice(0, 2).map((event, index) => (
+                              <GridEventCard key={index} event={event} past={true} />
+                            ))}
+                          </div>
+                          <div className=" grid md:hidden grid-cols-1 gap-10 transition w-fit px-[10px] sm:px-[17px] md:px-0  ">
+                            {pastEvents.slice(0, 1).map((event, index) => (
+                              <GridEventCard key={index} event={event} past={true} />
+                            ))}
+                          </div>
+                        </>
+                      </div>
+                    ) : (
+                      <div className="flex gap-y-1 flex-col h-[250px] justify-center items-center w-full">
+                        <h4
+                          className={` ${montserrat.className} text-2xl w-[196px] md:w-fit  font-semibold  text-center text-[#303030]`}
+                        >
+                          There’s nothing to show here.
+                        </h4>
+
+                        <p className="text-base text-[#767676] max-w-[220px] md:max-w-none w-[196px] md:w-fit text-center">
+                          <span
+                            className="font-bold text-primary-100 cursor-pointer"
+                            onClick={() => router.push('/create-events')}
+                          >
+                            create event
+                          </span>{' '}
+                          or{' '}
+                          <span
+                            className="font-bold text-primary-100 cursor-pointer"
+                            onClick={() => router.push('/explore')}
+                          >
+                            explore event
+                          </span>
+                        </p>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             )}
           </div>
-          <div className="calendarPart hidden md:flex lg:flex-col md:flex-row gap-8 md:pt-16 lg:pt-0 mx-auto">
+          {/* <div className="calendarPart hidden md:flex lg:flex-col md:flex-row gap-8 md:pt-16 lg:pt-0 mx-auto">
             <EventCalender3 events={[...combinedEvents, ...pastEvents]} />
 
             <div className=" flex flex-col text-[#FEFEFE] gap-y-6 text-base font-bold   ">
@@ -298,7 +318,7 @@ const Dashboard3: React.FC = () => {
                 Explore events <Discover />
               </Button>
             </div>
-          </div>
+          </div> */}
         </section>
       </div>
       {/* */}
