d11dc43d8a7b5de4b5d825d3c18c69fdb9f7e266
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2023-12-14 13:20:39 +0100

    Update index.tsx

--- pages/eventdashboard3/index.tsx
+++ pages/eventdashboard3/index.tsx
@@ -127,7 +127,7 @@ const Dashboard3: React.FC = () => {
               'text-primary-100 flex py-4 whitespace-nowrap justify-center !bg-[#FEFEFE] border border-primary-100 gap-x-2 w-[167px]'
             }
             title={''}
-            handleClick={() => router.push('/explore')}
+            handleClick={() => router.push('/loggedinexplore')}
           >
             Explore events <Discover />
           </Button>
