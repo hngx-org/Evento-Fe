5ff4d38513db7460fd23490cddf0970c271f8c9a
Nwonye-Michael <49059581+Nwonye-Michael@users.noreply.github.com>
2023-12-17 14:03:34 +0100

    responsivness for 404 and access denied page

--- pages/404.tsx
+++ pages/404.tsx
@@ -36,13 +36,13 @@ function Error404() {
         <div className=" border-b border-white-115 border-style: solid" />
 
         <section
-          className=" flex flex-col gap-9 sm:gap-[72px] min-h-[calc(100vh-105px)]  
+          className=" flex flex-col-reverse gap-9 sm:gap-[72px] min-h-[calc(100vh-105px)]  
       sm:mx-[105] items-center justify-center xl:flex-row-reverse xl:gap-[162px]"
         >
           <Image
             src={oops}
             alt="oops"
-            className="w-[300px] h-[300px] sm:w-[480px] sm:h-[480px] xl:w-[480px] xl:h-[480px]"
+            className="w-[320px] h-[240px] md:w-[440px] md:h-[440px] xl:w-[480px] xl:h-[480px]"
           />
           <div className=" flex flex-col justify-center items-center xl:items-start gap-6">
             <h2 className=" text-2xl md:text-[32px] xl:text-[45px] md:leading-[36px] xl:leading-[52px] sm:font-bold xl:text-left max-w-[504px] text-center font-semibold ">
--- pages/access-denied.tsx
+++ pages/access-denied.tsx
@@ -16,29 +16,38 @@ function AccessDenied() {
       <PlainHeader />
 
       <section
-        className=" flex flex-col gap-9 sm:gap-[72px] min-h-[calc(100vh-105px)]  
-      sm:mx-[105] items-center justify-center xl:flex-row-reverse xl:gap-[162px]"
+        className=" flex flex-col gap-[48px] md:gap-9 sm:gap-[72px] min-h-[680px] md:min-h-[calc(100vh-105px)]  
+      sm:mx-[105] items-center justify-center xl:flex-row xl:gap-[162px]"
       >
-        <Image
-          src={oops}
-          alt="Access-denied"
-          className="w-[300px] h-[300px] sm:w-[480px] sm:h-[480px] xl:w-[480px] xl:h-[480px]"
-        />
-        <div className=" flex flex-col justify-center items-center xl:items-start gap-6">
-          <h2 className=" text-2xl md:text-[32px] xl:text-[45px] md:leading-[36px] xl:leading-[52px] sm:font-bold xl:text-left max-w-[504px] text-center font-semibold ">
+        <div className=" flex flex-col justify-center items-center xl:items-start gap-4 md:gap-6 max-w-[358px] md:max-w-none">
+          <h2 className=" text-2xl md:text-[35px] xl:text-[45px] md:leading-[36px] xl:leading-[52px] sm:font-bold xl:text-left max-w-[504px] text-center font-semibold ">
             {title}
           </h2>
-          <p className="text-Grey-G300 max-w-[623px]  text-sm sm:text-2xl font-semibold text-left  ">
-            {text} <br />
+          <p className="text-Grey-G300 lg:max-w-[623px] md:max-w-[90%]  text-base md:text-2xl font-semibold md:text-left text-center">
+            {text} <br className="block md:hidden lg:block" />
             <span className="text-primary-100">
               {' '}
               <Link href={'/'}>Support@evento.com</Link>{' '}
             </span>
           </p>
+          <Link href="/">
+            <Button
+              styles={'flex py-4 px-[18px] whitespace-nowrap justify-center gap-x-2 lg:hidden text-white-N0 mt-8'}
+              title={'back home'}
+            >
+              <ArrowLeft size="32" color="#ffffff" /> Back to homepage
+            </Button>
+          </Link>
         </div>
+        <Image
+          src={oops}
+          alt="Access-denied"
+          className="w-[320px] h-[320px] sm:w-[480px] sm:h-[480px] xl:w-[480px] xl:h-[480px]"
+        />
       </section>
-
-      <Homefooter />
+      <div className="p-6 md:p-0">
+        <Homefooter />
+      </div>
     </main>
   );
 }
