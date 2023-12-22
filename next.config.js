248bdd026e1ac0f21dcb9c97cc78428370f1b7fa
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2023-12-22 11:25:25 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXTAUTH_SECRET: 'evento'
+  },
   images: {
     domains: ['res.cloudinary.com', 'example.com', 'images.unsplash.com', 'lh3.googleusercontent.com', 'img.evbuc.com'],
   },
