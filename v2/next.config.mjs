69ab417e0153c239ae14c30ebc1f0655ea3512e3
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-02-21 10:07:22 +0000

    update

--- v2/next.config.mjs
+++ v2/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+    images: {
+        remotePatterns: [
+            { hostname: 'lh3.googleusercontent.com' },
+            { hostname: 'ui-avatars.com' },
+        ]
+    },
+}
+
+
+
 
 export default nextConfig;
