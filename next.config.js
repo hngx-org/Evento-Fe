212579b5f0345771cf00bc1dd40fecab125f478c
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-02-23 14:07:24 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'example.com', 'images.unsplash.com', 'lh3.googleusercontent.com', 'img.evbuc.com'],
   },
+  exclude: ['v2'],
 };
 
 module.exports = nextConfig;
