82e9e08867c6006937b3536bda6bcc9b25c10634
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-02-23 15:24:34 +0000

    changes

---
+++ v1/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXTAUTH_SECRET: 'evento',
+  },
+  images: {
+    domains: ['res.cloudinary.com', 'example.com', 'images.unsplash.com', 'lh3.googleusercontent.com', 'img.evbuc.com'],
+  },
+};
+
+module.exports = nextConfig;
--- v1/pages/auth/sign-up.tsx
+++ v1/pages/auth/sign-up.tsx
@@ -1,3 +1,6 @@
+// @ts-nocheck
+
+
 'use client';
 
 import { Input } from '@ui/NewInput';
